63e714928efb4ae1679be342489c2712583ca68b
xiangjun <506402424@qq.com>
2017-04-17 16:58:17 +0800

    增加打包功能

--- webpack-config/output.js
+++ webpack-config/output.js
@@ -2,7 +2,7 @@ var path = require('path');
 var config = require('./config.js');
 
 var output = {
-  path: path.resolve(__dirname, '../dist/'),
+  path: config.distDir,//path.resolve(__dirname, '../dist/'),
   filename: 'js/[name]-[hash:8].js', //各页面模块对应的主要 js 文件命名
   chunkFilename: 'js/[name]-[id]-[chunkhash:8].js', // 分块后异步请求的 js 文件存放路径及命名
 }
